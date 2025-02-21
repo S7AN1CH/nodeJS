356cea102eeb2a20db77218c03d603d97bdd380b
Stas Urbanskyi <stanislav_urbanskyi@epam.com>
2025-02-21 10:44:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Module-2: Variables and Data Types
+
+## Hometask
+
+1. Fork this repository: `Variables and Data Types` by clicking `Start` button in Autocode
+2. Clone your newly created repo (do not need if you already did this in the scope of previous tasks): https://autocode.git.epam.com/<%your_gitlab_username%>/js-for-ste/
+3. Open your project folder
+4. Switch to another branch: `git checkout variables_and_data_types`
+5. To install all dependencies use [`npm install`](https://docs.npmjs.com/cli/install)
+6. Go to '.src/index.js' and do all the tasks from the file  
+7. Run `npm run test:dev` in the command line  
+8. You will see the number of passing and failing tests
+9. Push the results to `variables_and_data_types` branch
+
+## Submit to AutoCode
+1. Select your task (Variables and Data Types)
+2. Press the `Submit solution` button and enjoy, results will be available in few minutes
---
+++ test/index.js
@@ -0,0 +1,44 @@
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+
+import * as vars from '../src/index.js';
+
+describe('Task 2 - Variables and Data Types', () => {
+  it('int variable should be created', () => {
+    expect(vars.int).to.equal(1);
+  });
+
+  it('int2 variable should be created', () => {
+    expect(vars.int2).to.equal(2);
+  });
+
+  it('str variable should be created', () => {
+    expect(vars.str).to.equal('John');
+  });
+
+  it('str2 variable should be created', () => {
+    expect(vars.str2).to.equal('John Dou');
+  });
+
+  it('bool variable should be created', () => {
+    expect(vars.bool).to.equal(false);
+  });
+
+  it('arr variable should be created', () => {
+    const expected = [1, 2, 3, 4, 5];
+
+    expect(Array.isArray(vars.arr)).to.equal(true);
+    expect(vars.arr, 'arr variable is not equal to [1,2,3,4,5]').to.deep.equal(expected);
+  });
+
+  it('fifth variable should be created', () => {
+    expect(vars.arr[4]).to.equal(vars.fifth);
+  });
+
+  it('human variable should be created', () => {
+    expect(typeof vars.human).to.equal('object');
+    expect(vars.human.firstName).to.equal('John');
+    expect(vars.human.lastName).to.equal('Dou');
+    expect(vars.human.fullName).to.equal('John Dou');
+  });
+});
